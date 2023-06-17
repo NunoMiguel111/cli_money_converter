acede6bbc686486cae430d55fd3583ed762998bd
Nuno Miguel <up201806434@fe.up.pt>
2023-06-17 17:35:39 +0100

    added parse decimail function to convert string to decimal + unit tests

--- money/decimal.go
+++ money/decimal.go
@@ -1,5 +1,54 @@
 package money
 
-// Decimal is capable of storing a floating-point value
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	// ErrInvalidDecimal is returned if the decimal is malformed.
+	ErrInvalidDecimal = Error("unable to convert the decimal")
+	// ErrTooLarge is returned if the quantity is too large - this would cause floating point precision errors.
+	ErrTooLarge = Error("quantity over 10^12 is too large")
+)
+
+// Decimal is capable of storing a floating-point number with fixed precision.
+// example: 1.52 = 152 * 10^(-2) will be stored as {152, 2}
 type Decimal struct {
+	// subunits is the amount of subunits. Multiply it by the precision to get the real value
+	subunits int64
+	// Number of "subunits" in a unit, expressed as a power of 10
+	precision byte
+}
+
+// ParseDecimal converts a string into its Decimal representation.
+// It assumes  there is up to one decimal separator, and that the separator is "."(full stop character)
+func ParseDecimal(value string) (Decimal, error) {
+	intPart, fracPart, _ := strings.Cut(value, ".")
+
+	// maxDecimal is the number of digigts in a thousand billion.
+	const maxDecimal = 12
+
+	if len(intPart) > maxDecimal {
+		return Decimal{}, ErrTooLarge
+	}
+
+	subunits, err := strconv.ParseInt(intPart+fracPart, 10, 64)
+	if err != nil {
+		return Decimal{}, fmt.Errorf("%w: %s", ErrInvalidDecimal, err.Error())
+	}
+
+	precision := byte(len(fracPart))
+
+	return Decimal{subunits: subunits, precision: precision}, nil
+
+}
+
+// simplify the decimal ex: 1.50 -> 1.5
+func (d *Decimal) Simplify() {
+	for d.subunits%10 == 0 && d.precision > 0 {
+		d.precision--
+		d.subunits /= 10
+	}
 }
--- money/money_test.go
+++ money/money_test.go
@@ -1,36 +1,90 @@
-package money_test
+package money
 
 import (
-	"learngo-pockets/moneyconverter/money"
-	"reflect"
+	"errors"
+
 	"testing"
 )
 
-func TestConverter(t *testing.T) {
+func TestParseDecimal(t *testing.T) {
 	tt := map[string]struct {
-		amount   money.Amount
-		to       money.Currency
-		validate func(t *testing.T, got money.Amount, err error)
+		decimal  string
+		expected Decimal
+		err      error
 	}{
-		"34.98 USD to EUR": {
-			amount: money.Amount{},
-			to:     money.Currency{},
-			validate: func(t *testing.T, got money.Amount, err error) {
-				if err != nil {
-					t.Errorf("expected no error, got %s", err.Error())
-				}
-				expected := money.Amount{}
-				if !reflect.DeepEqual(got, expected) {
-					t.Errorf("expected: %v got: %v", expected, got)
-				}
+		"2 decimal digits": {
+			decimal: "1.52",
+			expected: Decimal{
+				subunits:  152,
+				precision: 2,
 			},
+			err: nil,
+		},
+		"no decimal digits": {
+			decimal: "10",
+			expected: Decimal{
+				subunits:  10, // Assuming the subunit is in cents (e.g., dollars to cents)
+				precision: 0,
+			},
+			err: nil,
+		},
+		"suffix 0 as decimal digits": {
+			decimal: "15.50",
+			expected: Decimal{
+				subunits:  1550,
+				precision: 2,
+			},
+			err: nil,
+		},
+		"prefix 0 as decimal digits": {
+			decimal: "0.75",
+			expected: Decimal{
+				subunits:  75,
+				precision: 2,
+			},
+			err: nil,
+		},
+		"multiple of 10": {
+			decimal: "100.00",
+			expected: Decimal{
+				subunits:  10000,
+				precision: 2,
+			},
+			err: nil,
+		},
+		"invalid decimal part": {
+			decimal:  "1.abc",
+			expected: Decimal{},
+			err:      ErrInvalidDecimal,
+		},
+		"Not a number": {
+			decimal:  "NaN",
+			expected: Decimal{},
+			err:      ErrInvalidDecimal,
+		},
+		"empty string": {
+			decimal:  "",
+			expected: Decimal{},
+			err:      ErrInvalidDecimal,
+		},
+		"too large": {
+			decimal:  "1234567890123",
+			expected: Decimal{},
+			err:      ErrTooLarge,
 		},
 	}
 
 	for name, tc := range tt {
 		t.Run(name, func(t *testing.T) {
-			got, err := money.Convert(tc.amount, tc.to)
-			tc.validate(t, got, err)
+			got, err := ParseDecimal(tc.decimal)
+
+			if !errors.Is(err, tc.err) {
+				t.Errorf("expected error %v, got %v", tc.err, err)
+			}
+
+			if got != tc.expected {
+				t.Errorf("expected %v, got %v", tc.expected, got)
+			}
 		})
 	}
 }
